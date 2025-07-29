a2a73ff730ee034a84d9a656d5b43d480be93038
tnchaudhari <tnchaudhari0@gmail.com>
2025-07-30 00:26:04 +0530

    Added Loading spinner while fetching the data from server.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,16 +2,20 @@ import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostListContext as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPost } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
 
   //useEffect is hook which we have used here to fetch the data when the page gets initially loaded. By giving [] empty braces after the function means this will only called once during initial stage.
   useEffect(() => {
+    setFetching(true);
     fetch('https://dummyjson.com/posts')
       .then(res => res.json())
       .then(data => {
         addInitialPost(data.posts);
+        setFetching(false);
       });
   }, [])
 
@@ -19,11 +23,12 @@ const PostList = () => {
 
   return (
     <>
+      {fetching && <LoadingSpinner />}
       {
-        postList.length === 0 && <WelcomeMessage />
+        !fetching && postList.length === 0 && <WelcomeMessage />
       }
       {
-        postList.map((post) => <Post key={post.id} post={post} />)
+        !fetching && postList.map((post) => <Post key={post.id} post={post} />)
       }
     </>
   )
